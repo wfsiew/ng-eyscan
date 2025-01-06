e9a72e882eb70f11530738e5ecd575486ae74d52
Siew W.F <wingfei.siew@nova-hub.com>
2025-01-06 15:51:55 +0800

    i

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { Component, Inject, OnDestroy, Renderer2, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { DOCUMENT } from '@angular/common';
@@ -12,14 +12,17 @@ import { AppTranslateService } from '../services/app-translate.service';
   styleUrl: './login.component.css',
   encapsulation: ViewEncapsulation.None
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
 
   lang = 'en';
+
+  private cssLink?: HTMLLinkElement;
 
   constructor(
     private router: Router,
     private translate: TranslateService,
     private appTranslate: AppTranslateService,
+    private renderer2: Renderer2,
     @Inject(DOCUMENT) private document: Document
   ) {
     const lang = appTranslate.getLang();
@@ -30,29 +33,20 @@ export class LoginComponent {
     this.initCss();
   }
 
+  ngOnDestroy() {
+    this.renderer2.removeChild(this.document.head, this.cssLink);
+  }
+
   initCss() {
     let cssPath = `assets/css/stylesheet_bg.css`;
     if (this.lang === 'cn') {
       cssPath = 'assets/css/stylesheet_bg_ch.css';
     }
 
-    const head = this.document.getElementsByTagName('head')[0];
-    let cssLink = this.document.getElementById(
-      'stylesheet_bg'
-    ) as HTMLLinkElement;
-    if (cssLink) {
-      cssLink.href = cssPath;
-    }
-
-    else {
-      const style = this.document.createElement('link');
-      style.id = 'stylesheet_bg';
-      style.rel = 'stylesheet';
-      style.type = 'text/css';
-      style.href = cssPath;
-
-      head.appendChild(style);
-    }
+    this.cssLink = this.renderer2.createElement('link') as HTMLLinkElement;
+    this.renderer2.appendChild(this.document.head, this.cssLink);
+    this.renderer2.setProperty(this.cssLink, 'rel', 'stylesheet');
+    this.renderer2.setProperty(this.cssLink, 'href', cssPath);
   }
 
   onForgotPwd() {
@@ -61,16 +55,7 @@ export class LoginComponent {
   }
 
   onSubmit() {
-    this.router.navigate(['/home']).then(() => {
-      const cssLink = this.document.getElementById(
-        'stylesheet_bg'
-      ) as HTMLLinkElement;
-      if (cssLink) {
-        const head = this.document.getElementsByTagName('head')[0];
-        head.removeChild(cssLink);
-      }
-      
-    });
+    this.router.navigate(['/home']);
     return false;
   }
 
