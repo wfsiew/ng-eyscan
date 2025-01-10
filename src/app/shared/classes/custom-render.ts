59a2de4d7be742456474594f8f9d160934479390
Siew Wing Fei <wingfei.siew@nova-hub.com>
2025-01-10 20:06:44 +0800

    k

--- src/app/shared/classes/custom-render.ts
+++ src/app/shared/classes/custom-render.ts
@@ -4,9 +4,11 @@ export class CustomRender {
 
   constructor(protected renderer: Renderer2) {
     this.renderer.addClass(document.body, 'main-body');
+    this.renderer.addClass(document.body, 'body-main-margin');
   }
 
   destroy() {
     this.renderer.removeClass(document.body, 'main-body');
+    this.renderer.removeClass(document.body, 'body-main-margin');
   }
 }
