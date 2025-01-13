d8d917bb532fdfefd2972cf5efe3911356820907
Siew W.F <wingfei.siew@nova-hub.com>
2025-01-13 16:36:34 +0800

    i

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, Inject, OnDestroy, Renderer2, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService, ModalDirective } from 'ngx-bootstrap/modal';
 import { TranslateService } from '@ngx-translate/core';
 import { DOCUMENT } from '@angular/common';
 import { AppTranslateService } from '../services/app-translate.service';
-import { ErrorBoxComponent } from '../shared/components/error-box/error-box.component';
+import { ErrorBoxComponent } from 'src/app/shared/components/error-box/error-box.component';
 
 @Component({
   selector: 'app-login',
@@ -16,22 +17,24 @@ import { ErrorBoxComponent } from '../shared/components/error-box/error-box.comp
 })
 export class LoginComponent implements OnDestroy {
 
-  lang = 'en';
+  mform?: UntypedFormGroup;
+  bsModalRef?: BsModalRef;
 
   private cssLink?: HTMLLinkElement;
-  bsModalRef?: BsModalRef;
 
   constructor(
     private router: Router,
     private modalService: BsModalService,
+    private fb: UntypedFormBuilder,
     private translate: TranslateService,
     private appTranslate: AppTranslateService,
     private renderer2: Renderer2,
     @Inject(DOCUMENT) private document: Document
   ) {
+    this.createForm();
     const lang = appTranslate.getLang();
     if (lang) {
-      this.lang = lang;
+      this.mform?.patchValue({ inLang: lang });
     }
 
     this.initCss();
@@ -53,12 +56,24 @@ export class LoginComponent implements OnDestroy {
     this.renderer2.setProperty(this.cssLink, 'href', cssPath);
   }
 
+  createForm() {
+    this.mform = this.fb.group({
+      inEmail: ['', [Validators.required]],
+      inPassword: ['', [Validators.required]],
+      inLang: ['en']
+    });
+  }
+
   onForgotPwd() {
     this.router.navigate(['/forgotpwd'], { skipLocationChange: true });
     return false;
   }
 
   onSubmit() {
+    if (this.mform?.invalid) {
+      this.mform?.markAllAsTouched();
+      return;
+    }
     // const initialState = {
     //   err: '...'
     // };
@@ -70,13 +85,30 @@ export class LoginComponent implements OnDestroy {
     // });
 
     this.router.navigate(['/application/home']);
-    return false;
   }
 
   onChangeLang(ev: any) {
-    this.lang = ev.target.value;
+    this.mform?.patchValue({ inLang: ev.target.value });
     this.translate.use(ev.target.value);
-    this.appTranslate.setLang(this.lang);
+    this.appTranslate.setLang(ev.target.value);
     location.href = location.href;
+  }
+
+  get lang() {
+    const f = this.mform?.value;
+    return f.inLang;
+  }
+
+  get f() {
+    return this.mform?.controls;
+  }
+
+  invalid(s: string) {
+    const m = this.mform?.controls[s];
+    if (!m) {
+      return false;
+    }
+
+    return m.invalid && (m.dirty || m.touched);
   }
 }
